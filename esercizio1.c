d1bc169d4f334df1c4ff8d01f5d1c6c04bcd1589
miryrusso <russo.miriana@studium.unict.it>
2023-10-16 19:53:47 +0200

    Primo Esercizio

---
+++ esercizio1.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: 06 ott 2023
+ *      Author: miriana
+ */
+
+
+#include "stm32_unict_lib.h"
+
+void setup(void)
+{
+	GPIO_init(GPIOB);
+	GPIO_config_output(GPIOB, 0);
+}
+
+void loop(void)
+{
+	GPIO_write(GPIOB, 0, 1);
+	delay_ms(500);
+	GPIO_write(GPIOB, 0, 0);
+	delay_ms(500);
+}
+
+int main()
+{
+	setup();
+	for(;;) {
+		loop();
+	}
+	return 0;
+}
+
